ace37615c500b98daa775c92a305961b4527fa7d
ephong93 <ephong93@gmail.com>
2022-07-18 00:03:13 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-	alias vi=nvim
-	alias vim=nvim
+    alias vi=nvim
+    alias vim=nvim
+	if type -q exa
+		alias ll "exa -l -g --icons"
+		alias lla "ll -a"
+	end
 end
+
