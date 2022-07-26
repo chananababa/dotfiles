16a2698d94f54e3533e61d33e95c6f53b216ec45
ephong93 <ephong93@gmail.com>
2022-07-26 23:46:59 +0900

    feat(nvim): show diagnostic messages in float window

--- .config/nvim/lua/lsp/diagnostic.lua
+++ .config/nvim/lua/lsp/diagnostic.lua
@@ -3,4 +3,16 @@ vim.diagnostic.config({
         source = "always",
         prefix = "●",
     },
+    float = {
+        source = "always",
+    },
 })
+
+vim.api.nvim_create_autocmd({
+    "CursorMoved",
+    "CursorMovedI",
+}, {
+    callback = function()
+        vim.diagnostic.open_float()
+    end,
+}
