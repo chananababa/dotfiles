157775c097b988fde6a273fce6462bbd1ac516e7
ephong93 <ephong93@gmail.com>
2022-08-03 00:29:00 +0900

    ✨ feat(fish): add alias g git

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    alias vi=nvim
-    alias vim=nvim
+    alias vi nvim
+    alias vim nvim
+    alias g git
 	if type -q exa
 		alias ll "exa -l -g --icons"
 		alias lla "ll -a"
 	end
+    if type -q zoxide
+        zoxide init fish | source
+    end
 	if test (uname) = Darwin
 		fish_add_path /opt/homebrew/bin
 	end
-
 end
 
