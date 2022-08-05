2582b02ad776fb9a6166809d3123aac60a07d42e
chananababa <ephong93@gmail.com>
2022-08-05 23:55:51 +0900

    ✨ feat(nvim): add telescope config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,27 @@
-require("telescope").setup({
+local telescope = require("telescope")
+
+telescope.setup({
     defaults = {
         file_ignore_patterns = { "node_modules", "__pycache__" },
     },
     pickers = {
         find_files = {
             hidden = true,
+        },
+    },
+    tele_tabby = {
+        use_highlighter = true,
+    },
+})
+
+telescope.load_extension("zoxide")
+
+require("telescope._extensions.zoxide.config").setup({
+    mappings = {
+        default = {
+            after_action = function(selection)
+                vim.cmd("tcd " .. selection.path)
+            end,
         },
     },
 })
