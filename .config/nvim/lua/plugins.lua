8995a1a33e979f8ccae7da4adaa167cb7d434ac1
chananababa <ephong93@gmail.com>
2022-08-06 23:50:31 +0900

    ✨ feat(nvim): add nvim-ts-autotag for html auto pair

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -14,16 +14,14 @@ Plug("williamboman/nvim-lsp-installer")
 Plug("nvim-lua/lsp-status.nvim")
 Plug("nvim-lua/plenary.nvim")
 Plug("nvim-telescope/telescope.nvim", { tag = "0.1.0" })
-Plug(
-    "nvim-telescope/telescope-fzf-native.nvim",
-    {
-        ["do"] = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-    }
-)
+Plug("nvim-telescope/telescope-fzf-native.nvim", {
+    ["do"] = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+})
 Plug("hrsh7th/nvim-cmp")
 Plug("saadparwaiz1/cmp_luasnip")
 Plug("L3MON4D3/LuaSnip")
 Plug("windwp/nvim-autopairs")
+Plug("windwp/nvim-ts-autotag")
 Plug("jose-elias-alvarez/null-ls.nvim")
 Plug("hrsh7th/cmp-nvim-lsp")
 Plug("nvim-lua/popup.nvim")
