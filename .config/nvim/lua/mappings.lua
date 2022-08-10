f8d039485784e47770d52ff4ff65502180504f8c
chananababa <ephong93@gmail.com>
2022-08-10 12:52:39 +0900

    ♻️ refactor(nvim): replace vim.api function with vim.keymap

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,32 +1,32 @@
 vim.g.mapleader = " "
-vim.api.nvim_set_keymap("n", "<leader>tt", "<cmd>NvimTreeToggle<cr>", { noremap = true })
-vim.api.nvim_set_keymap(
+vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<cr>", { noremap = true })
+vim.keymap.set(
     "n",
     "<leader>ff",
     "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
     { noremap = true }
 )
-vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { noremap = true })
-vim.api.nvim_set_keymap(
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { noremap = true })
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { noremap = true })
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { noremap = true })
+vim.keymap.set(
     "n",
     "<leader>cd",
     ":lua require'telescope'.extensions.zoxide.list{}<CR>",
     { noremap = true, silent = true }
 )
 
-vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "<leader>gd", "<cmd>Lspsaga preview_definition<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "<leader>gh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gs", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gs", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gd", "<cmd>Lspsaga preview_definition<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
 
 vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
 vim.keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
 
 local action = require("lspsaga.codeaction")
 vim.keymap.set("n", "<leader>ca", action.code_action, { silent = true })
-vim.api.nvim_set_keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
+vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
 
 vim.keymap.set("n", "<M-d>", "<cmd>Lspsaga open_floaterm<CR>", { silent = true })
 vim.keymap.set("t", "<M-d>", "<cmd>Lspsaga close_floaterm<CR>", { silent = true })
