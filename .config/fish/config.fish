a8144c66216606b230f715fa5c1701e0cbf58d01
chananababa <ephong93@gmail.com>
2022-08-12 11:00:43 +0900

    ✨ feat(fish): add python bin folder to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,21 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
     alias vi nvim
     alias vim nvim
     alias g git
-	if type -q exa
-		alias ll "exa -l -g --icons"
-		alias lla "ll -a"
-	end
+    if type -q exa
+        alias ll "exa -l -g --icons"
+        alias lla "ll -a"
+    end
     if type -q zoxide
         zoxide init fish | source
     end
-	if test (uname) = Darwin
-		fish_add_path /opt/homebrew/bin
-	end
+    if test (uname) = Darwin
+        fish_add_path /opt/homebrew/bin
+        fish_add_path ~/Library/Python/3.8/bin
+    end
+    if test (uname) = Linux
+        fish_add_path ~/.local/bin
+    end
 end
-
