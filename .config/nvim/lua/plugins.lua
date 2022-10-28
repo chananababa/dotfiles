2e363c3b458c0219649d84cef2c67f5f26ae0a3e
chananababa <ephong93@gmail.com>
2022-10-29 00:53:18 +0900

    fix(nvim): turn off current_line_blame

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -51,7 +51,7 @@ return require("packer").startup(function(use)
         "lewis6991/gitsigns.nvim",
         config = function()
             require("gitsigns").setup({
-                current_line_blame = true,
+                current_line_blame = false,
                 current_line_blame_opts = {
                     delay = 300,
                 },
