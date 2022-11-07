1bc398bbb9225779051de019c988243ce47c0907
ephong93 <ephong93@gmail.com>
2022-11-07 10:19:06 +0900

    feat(nvim): map jk to esc

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,6 +1,13 @@
+-- map leader to Space
 vim.g.mapleader = " "
+
+-- for calmelCaseMotion
 vim.g.camelcasemotion_key = "<leader>"
+
+-- Toggle nvim-tree
 vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle .<cr>", { noremap = true })
+
+-- telescope
 vim.keymap.set(
     "n",
     "<leader>ff",
@@ -18,9 +25,11 @@ vim.keymap.set(
     { noremap = true, silent = true }
 )
 
+-- tab
 vim.keymap.set("n", "<leader>tn", ":tabnew<CR>")
 vim.keymap.set("n", "<leader>tr", ":TabRename ")
 
+-- lspsaga
 vim.keymap.set("n", "gs", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
 vim.keymap.set("n", "gs", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
 vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
@@ -37,6 +46,8 @@ vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
 vim.keymap.set("n", "<M-d>", "<cmd>Lspsaga open_floaterm<CR>", { silent = true })
 vim.keymap.set("t", "<M-d>", "<cmd>Lspsaga close_floaterm<CR>", { silent = true })
 
+
+-- move line block
 vim.keymap.set("n", "<M-j>", ":m .+1<CR>==", { silent = true, noremap = true })
 vim.keymap.set("n", "<M-k>", ":m .-2<CR>==", { silent = true, noremap = true })
 vim.keymap.set("i", "<M-j>", "<Esc>:m .+1<CR>==gi", { silent = true, noremap = true })
@@ -44,4 +55,9 @@ vim.keymap.set("i", "<M-k>", "<Esc>:m .-2<CR>==gi", { silent = true, noremap = t
 vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv", { silent = true, noremap = true })
 vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv", { silent = true, noremap = true })
 
+-- copy to clipboard
 vim.keymap.set("v", "<leader>c", ":OSCYank<CR>", { noremap = true })
+
+-- map jk to ESC
+vim.keymap.set("i", "jk", "<ESC>", { noremap = true })
+vim.keymap.set("i", "kj", "<ESC>", { noremap = true })
