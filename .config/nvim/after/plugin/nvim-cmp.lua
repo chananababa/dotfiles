37119c367d6a8b20cc42c56823ee6a93d6bac6ca
chananababa <ephong93@gmail.com>
2022-11-19 00:33:16 +0900

    feat(nvim): add snippet settings

--- .config/nvim/after/plugin/nvim-cmp.lua
+++ .config/nvim/after/plugin/nvim-cmp.lua
@@ -1,6 +1,8 @@
 -- Add additional capabilities supported by nvim-cmp
-local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
-if not cmp_nvim_lsp_status then return end
+local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not cmp_nvim_lsp_status then
+    return
+end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
@@ -24,7 +26,6 @@ local cmp_status, cmp = pcall(require, "cmp")
 if not cmp_status then
     return
 end
-
 
 local kind_icons = {
     Text = "",
@@ -51,7 +52,7 @@ local kind_icons = {
     Struct = "",
     Event = "",
     Operator = "",
-    TypeParameter = ""
+    TypeParameter = "",
 }
 
 cmp.setup({
@@ -101,7 +102,7 @@ cmp.setup({
     formatting = {
         format = function(entry, vim_item)
             -- Kind icons
-            vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+            vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
             -- Source
             vim_item.menu = ({
                 buffer = "[Buffer]",
@@ -111,7 +112,7 @@ cmp.setup({
                 latex_symbols = "[LaTeX]",
             })[entry.source.name]
             return vim_item
-        end
+        end,
     },
 })
 
@@ -123,3 +124,8 @@ autopairs.setup({})
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+-- Snippet
+require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip").filetype_extend("javascriptreact", { "javascript" })
+require("luasnip").filetype_extend("typescriptreact", { "javascript", "typescript" })
