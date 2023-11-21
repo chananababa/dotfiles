946752d03336b449066939ace5b13d1298bdee6b
gnoeyp <silver.hong@bucketplace.net>
2023-11-21 17:06:20 +0900

    update lsp

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -23,37 +23,41 @@ local eslint_root_patterns = {
 return {
   "stevearc/conform.nvim",
   dependencies = { "mason.nvim" },
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      javascript = { { "prettierd", "prettier" }, { "eslint_d", "eslint" } },
-      typescript = { { "prettierd", "prettier" }, { "eslint_d", "eslint" } },
-    },
-    formatters = {
-      stylua = {
-        prepend_args = { "--indent-type=spaces", "--indent-width=2" },
+  opts = function()
+    return {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { { "prettierd", "prettier" }, { "eslint_d", "eslint" } },
+        javascriptreact = { { "prettierd", "prettier" }, { "eslint_d", "eslint" } },
+        typescript = { { "prettierd", "prettier" }, { "eslint_d", "eslint" } },
+        typescriptreact = { { "prettierd", "prettier" }, { "eslint_d", "eslint" } },
       },
-    },
-    format_on_save = {
-      -- These options will be passed to conform.format()
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-  },
-  config = function(_, opts)
-    local conform = require("conform")
-    conform.setup(opts)
-    conform.formatters.prettierd = {
-      cwd = require("conform.util").root_file(prettier_root_patterns),
-    }
-    conform.formatters.prettier = {
-      cwd = require("conform.util").root_file(prettier_root_patterns),
-    }
-    conform.formatters.eslint_d = {
-      cwd = require("conform.util").root_file(eslint_root_patterns),
-    }
-    conform.formatters.eslint = {
-      cwd = require("conform.util").root_file(eslint_root_patterns),
+      formatters = {
+        stylua = {
+          prepend_args = { "--indent-type=spaces", "--indent-width=2" },
+        },
+        prettierd = {
+          cwd = require("conform.util").root_file(prettier_root_patterns),
+          require_cwd = true,
+        },
+        prettier = {
+          cwd = require("conform.util").root_file(prettier_root_patterns),
+          require_cwd = true,
+        },
+        eslint_d = {
+          cwd = require("conform.util").root_file(eslint_root_patterns),
+          require_cwd = true,
+        },
+        eslint = {
+          cwd = require("conform.util").root_file(eslint_root_patterns),
+          require_cwd = true,
+        },
+      },
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = false,
+      },
     }
   end,
 }
