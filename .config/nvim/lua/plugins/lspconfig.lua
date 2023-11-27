482249807751d0ca027588e17499cbc3a5527bde
홍은평 <ephong93@gmail.com>
2023-11-28 03:43:17 +0900

    update lspconfig

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,5 +1,28 @@
 local required_servers = { "lua_ls", "eslint", "tsserver", "pyright", "tailwindcss", "html", "prismals" }
 
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    bufnr = bufnr,
+  })
+end
+
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+-- add to your shared on_attach callback
+local on_attach = function(client, bufnr)
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        lsp_formatting(bufnr)
+      end,
+    })
+  end
+end
+
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
@@ -33,6 +56,9 @@ return {
             },
           },
         },
+      },
+      prismals = {
+        on_attach = on_attach,
       },
     }, {
       __index = function()
